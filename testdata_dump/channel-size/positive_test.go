9497324366132d60f56485bdebf7bd513ff17f2b
Beltran Aceves <beltranaceves@gmail.com>
2026-04-11 14:51:03 +0200

    [DEL]: legacy rules dir

--- testdata_dump/channel-size/positive_test.go
+++
@@ -1,9 +0,0 @@
-// Auto-generated test cases for rule
-// Positive = should FAIL lint (Bad code)
-// Negative = should PASS lint (Good code)
-
-package testdata
-
-// Example 1
-// Ought to be enough for anybody!
-c := make(chan int, 64)
--- testdata_dump/struct-zero/negative_test.go
+++
@@ -1,8 +0,0 @@
-// Auto-generated test cases for rule
-// Positive = should FAIL lint (Bad code)
-// Negative = should PASS lint (Good code)
-
-package testdata
-
-// Example 1
-var user User
